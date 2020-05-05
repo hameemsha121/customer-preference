125ee58f912568531887b38ec2665378453fe608
Malavika Unni <malavika.unni30@gmail.com>
2020-05-05 20:31:07 +0530

    updated

--- get-customer-id-service/src/main/java/com/cts/superstore/repository/GetCustomerIdRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cts.superstore.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cts.superstore.entity.CustomerEntity;
-
-public interface GetCustomerIdRepository extends JpaRepository<CustomerEntity,Integer>{
-
-}
--- get-customer-id-service/src/main/java/com/cts/superstore/service/GetCustomerIdService.java
+++
@@ -1,9 +0,0 @@
-package com.cts.superstore.service;
-
-import com.cts.superstore.model.Customer;
-
-public interface GetCustomerIdService {
-	
-	public Customer getCustomerById(int id);
-
-}
